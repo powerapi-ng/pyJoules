4fc3a0ed0b899f8ddb710d591b9bf28804183c43
adazemar <arthur.d-azemar@inria.fr>
2020-07-10 17:39:27 +0200

    feat: add EnergyTrace

--- pyJoules/energy_sample.py
+++ pyJoules/energy_sample.py
@@ -18,7 +18,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 from dataclasses import dataclass
-from typing import Dict
+from typing import Dict, Any, List, Callable
+from functools import reduce
+from operator import and_
+
 
 @dataclass
 class EnergySample:
@@ -36,3 +39,92 @@ class EnergySample:
     tag: str
     duration: float
     energy: Dict[str, float]
+
+
+class EnergyTrace:
+    """
+    Trace of all EnergySample collected by a meter
+    """
+    def __init__(self, samples: List[EnergySample]):
+        self._samples = []
+        for sample in samples:
+            self._samples.append(sample)
+
+    def _get_sample_from_tag(self, tag):
+        for sample in self._samples:
+            print(sample)
+            if sample.tag == tag:
+                return sample
+
+    def __getitem__(self, key: Any) -> EnergySample:
+        """
+        Return the n-th EnergySample on the trace or the first sample with the given tag
+        :param key: integer to get the n-th EnergySample or the tag of the needed sample
+        :return: An EnergySample
+        :raise KeyError: if no sample match the given tag
+        :raise IndexError: if no sample match the given index
+        """
+        if isinstance(key, int):
+            if key > len(self._samples):
+                raise IndexError('Trace index out of range : ' + str(key))
+            return self._samples[key]
+
+        sample = self._get_sample_from_tag(key)
+        if sample is None:
+            raise KeyError('this tag doesn\'t match any sample : ' + str(key))
+        return sample
+
+    def __iter__(self):
+        """
+        Iterate on the trace's samples
+        """
+        return self._samples.__iter__()
+
+    def __len__(self) -> int:
+        """
+        return the number of sample in the trace
+        """
+        return len(self._samples)
+
+    def remove_idle(self, idle: List[Dict[str, float]]):
+        """
+        substract idle energy values from the current trace
+        :param idle: list of idle consumption values to substract to current trace
+                     idle consumption values must be grouped in a dictionary with their domain as key
+        :raise ValueError: if the number of values in the list doesn't match the number of sample in the trace
+                           or if a domain of the trace is not in the idle values
+        """
+        if len(idle) != len(self._samples):
+            raise ValueError('idle list havn\'t the same length than the trace')
+
+        for idle_energy, sample in zip(idle, self._samples):
+            for domain in sample.energy:
+                if domain not in idle_energy:
+                    raise ValueError('domain not present in idle values : ' + domain)
+                sample.energy[domain] -= idle_energy[domain]
+
+    def _sample_havnt_negative_values(self, sample):
+        for val in sample.energy.values():
+            if val < 0:
+                return False
+        return True
+
+    def clean_data(self, guards: List[Callable[[EnergySample], bool]] = []):
+        """
+        Remove sample with negative energy values from the trace
+
+        Guards can be added to specify rules to remove sample
+
+        :param guards: list of function that is used as rules to remove samples. A guard is a function that take a
+                       sample as parameter and return True if it must be keept in the trace, False otherwise
+        """
+
+        extended_guards = [lambda s: self._sample_havnt_negative_values(s)] + guards
+
+        valid_samples = []
+        for sample in self._samples:
+
+            validity_list = [guard(sample) for guard in extended_guards]
+            if reduce(and_, validity_list):
+                valid_samples.append(sample)
+        self._samples = valid_samples
---
+++ tests/unit/energy_sample/test_EnergyTrace.py
@@ -0,0 +1,188 @@
+# MIT License
+# Copyright (c) 2019, INRIA
+# Copyright (c) 2019, University of Lille
+# All rights reserved.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import pytest
+
+from pyJoules.energy_sample import EnergyTrace, EnergySample
+
+
+SAMPLE_1 = EnergySample('123', 'tag1', 10, {'domain1': 1, 'domain2': 2})
+SAMPLE_2 = EnergySample('456', 'tag2', 20, {'domain2': 2, 'domain3': 3})
+
+IDLE_1 = {'domain1': 1, 'domain2': 1}
+IDLE_2 = {'domain2': 1, 'domain3': 2}
+
+NEGATIVE_SAMPLE_1 = EnergySample('123', 'tag1', 10, {'domain1': -1, 'domain2': 2})
+NEGATIVE_SAMPLE_2 = EnergySample('456', 'tag2', 20, {'domain2': 2, 'domain3': -3})
+
+
+@pytest.fixture
+def trace_with_one_sample():
+    return EnergyTrace([SAMPLE_1])
+
+
+@pytest.fixture
+def trace_with_two_sample():
+    return EnergyTrace([SAMPLE_1, SAMPLE_2])
+
+
+@pytest.fixture
+def negative_trace():
+    return EnergyTrace([NEGATIVE_SAMPLE_1, NEGATIVE_SAMPLE_2])
+
+
+@pytest.fixture
+def semi_negative_trace():
+    return EnergyTrace([SAMPLE_1, NEGATIVE_SAMPLE_2])
+
+
+##########
+# LENGTH #
+##########
+def test_length_of_one_element_trace_is_one(trace_with_one_sample):
+    assert len(trace_with_one_sample)
+
+
+def test_length_of_two_elements_trace_is_two(trace_with_two_sample):
+    assert len(trace_with_two_sample)
+
+
+##################
+# LIST INTERFACE #
+##################
+
+def test_create_trace_with_one_sample_and_get_second_element_must_raise_IndexError(trace_with_one_sample):
+    with pytest.raises(IndexError):
+        trace_with_one_sample[1]
+
+
+def test_create_trace_with_one_sample_and_get_first_element_must_return_first_element(trace_with_one_sample):
+    assert trace_with_one_sample[0] == SAMPLE_1
+
+
+def test_create_trace_with_two_sample_and_get_second_element_must_return_second_element(trace_with_two_sample):
+    assert trace_with_two_sample[1] == SAMPLE_2
+
+
+def test_create_a_trace_from_a_localy_defined_list_and_modify_the_list_musnt_modify_the_trace():
+    locale_list = [SAMPLE_1]
+    trace = EnergyTrace(locale_list)
+    locale_list[0] = 0
+    assert trace[0] != 0
+
+
+##################
+# DICT INTERFACE #
+##################
+def test_create_trace_with_one_sample_and_get_bad_tag_must_raise_KeyError(trace_with_one_sample):
+    with pytest.raises(KeyError):
+        trace_with_one_sample['bad_tag']
+
+
+def test_create_trace_with_one_sample_and_get_tag_of_the_first_element_must_return_first_element(trace_with_one_sample):
+    assert trace_with_one_sample['tag1'] == SAMPLE_1
+
+
+def test_create_trace_with_one_sample_and_get_tag_of_the_second_element_must_return_second_element(trace_with_two_sample):
+    assert trace_with_two_sample['tag2'] == SAMPLE_2
+
+
+###########
+# ITERATE #
+###########
+def test_create_trace_with_one_sample_and_iter_on_it_must_iter_on_one_element(trace_with_one_sample):
+    l = []
+    for s in trace_with_one_sample:
+        l.append(s)
+    assert l[0] == SAMPLE_1
+
+def test_create_trace_with_two_sample_and_iter_on_it_must_iter_on_two_elements(trace_with_two_sample):
+    l = []
+    for s in trace_with_two_sample:
+        l.append(s)
+    assert l[0] == SAMPLE_1
+    assert l[1] == SAMPLE_2
+
+
+###############
+# REMOVE_IDLE #
+###############
+def test_create_trace_with_two_element_and_remove_idle_with_one_value_must_raise_ValueError(trace_with_two_sample):
+    with pytest.raises(ValueError):
+        trace_with_two_sample.remove_idle([IDLE_1])
+
+
+def test_create_trace_with_two_element_and_remove_idle_with_three_value_must_raise_ValueError(trace_with_two_sample):
+    with pytest.raises(ValueError):
+        trace_with_two_sample.remove_idle([IDLE_1, IDLE_1, IDLE_2])
+
+
+def test_create_trace_with_two_element_and_remove_idle_with_value_with_bad_tag_must_raise_ValueError(trace_with_two_sample):
+    with pytest.raises(ValueError):
+        trace_with_two_sample.remove_idle([IDLE_2, IDLE_1])
+
+
+def test_create_trace_with_two_element_and_remove_idle_must_return_good_values(trace_with_two_sample):
+    trace_with_two_sample.remove_idle([IDLE_1, IDLE_2])
+    assert trace_with_two_sample[0].energy['domain1'] == 0
+    assert trace_with_two_sample[0].energy['domain2'] == 1
+    assert trace_with_two_sample[1].energy['domain2'] == 1
+    assert trace_with_two_sample[1].energy['domain3'] == 1
+
+
+##############
+# CLEAN_DATA #
+##############
+def test_clean_trace_without_negative_value_must_return_the_same_trace(trace_with_two_sample):
+    trace_with_two_sample.clean_data()
+    assert len(trace_with_two_sample) == 2
+
+
+def test_create_trace_with_two_element_with_negative_energy_values_and_clean_data_must_remove_all_elements(negative_trace):
+    negative_trace.clean_data()
+    assert len(negative_trace) == 0
+
+
+def test_create_trace_with_two_element_with_one_element_with_negative_energy_values_and_clean_data_must_remove_one_element(semi_negative_trace):
+    semi_negative_trace.clean_data()
+    assert len(semi_negative_trace) == 1
+    assert semi_negative_trace[0] == SAMPLE_1
+
+
+def test_clean_with_guard_a_trace_without_negative_sample_but_a_bad_sample_must_remove_this_sample(trace_with_two_sample):
+    trace_with_two_sample.clean_data(guards=[lambda sample: False if 'domain3' in sample.energy else True])
+    assert len(trace_with_two_sample) == 1
+    assert trace_with_two_sample[0] == SAMPLE_1
+
+
+def test_clean_with_guard_a_trace_without_negative_sample_but_two_bad_sample_must_remove_all_samples(trace_with_two_sample):
+    trace_with_two_sample.clean_data(guards=[lambda sample: False if 'domain2' in sample.energy else True])
+    assert len(trace_with_two_sample) == 0
+
+
+def test_clean_with_guard_a_trace_with_a_negative_and_bad_sample_must_remove_this_samples():
+    trace = EnergyTrace([SAMPLE_1, NEGATIVE_SAMPLE_2])
+    trace.clean_data(guards=[lambda sample: False if 'domain3' in sample.energy else True])
+    assert len(trace) == 1
+    assert trace[0] == SAMPLE_1
+
+def test_clean_with_guard_a_trace_with_a_negative_sample_and_a_bad_sample_must_remove_all_samples():
+    trace = EnergyTrace([SAMPLE_1, NEGATIVE_SAMPLE_2])
+    trace.clean_data(guards=[lambda sample: False if 'domain1' in sample.energy else True])
+    assert len(trace) == 0
